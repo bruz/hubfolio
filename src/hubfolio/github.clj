20f196d58bf74e42ae57109af54cf53af68fa5e7
Bruz Marzolf <bruz@bruzilla.com>
2014-12-25 22:52:44 -0800

    Move github call caching into github namespace

--- src/hubfolio/github.clj
+++ src/hubfolio/github.clj
@@ -1,5 +1,20 @@
 (ns hubfolio.github
-  (:require [tentacles.repos :as repos]))
+  (:require [tentacles.repos :as repos]
+            [hubfolio.cache :refer [cache]]))
 
-(defn user-repos [auth user]
-  (repos/user-repos user (conj auth {:type "all" :all-pages true})))
+(defn request [resource args]
+  (let [response (apply resource args)]
+    (println "call-remaining: " (-> response meta :api-meta :call-remaining))
+    (println "etag: " (-> response meta :api-meta :etag))
+    response))
+
+(defn cached [resource cache-config key options & xs]
+  (let [args (concat xs [options])]
+    (cache cache-config key
+           (request resource args))))
+
+(defn user-repos [conn username]
+  (let [{:keys [cache-config github-auth]} conn
+        key (str "repos:" username)
+        options (conj github-auth {:type "all" :all-pages true})]
+    (cached repos/user-repos cache-config key options username)))
--- src/hubfolio/statistics.clj
+++ src/hubfolio/statistics.clj
@@ -3,19 +3,8 @@
             [hubfolio.github :as github]
             [hubfolio.cache :refer [cache]]))
 
-(defn github [resource github-auth & xs]
-  (let [response (apply resource github-auth xs)]
-    (println "call-remaining: " (-> response meta :api-meta :call-remaining))
-    response))
-
-(defn cached [resource conn key & xs]
-  (let [{:keys [cache-config github-auth]} conn]
-    (cache cache-config key
-      (apply github resource github-auth xs))))
-
 (defn repos [username conn]
-  (let [key (str "repos:" username)]
-    (cached github/user-repos conn key username)))
+  (github/user-repos conn username))
 
 (defrecord Statistics [github-auth cache-config])
 
